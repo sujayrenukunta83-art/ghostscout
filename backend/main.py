291d3de4ed6eda7560330cd067b1ee2df5fd2414
sujayrenukunta83-art <sujayrenukunta83@gmail.com>
2026-05-31 19:18:43 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -25,11 +25,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://ghostscout-aj08ulm3p-sujayrenukunta83-arts-projects.vercel.app/"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
